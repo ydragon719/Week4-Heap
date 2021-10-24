b81179d3a398b1da0de5bcbc101f01117c2b3428
ydragon719 <85536991+ydragon719@users.noreply.github.com>
2021-10-24 20:43:00 +0800

    Create leftistheap.h

---
+++ LeftistHeap/leftistheap.h
@@ -0,0 +1,27 @@
+#ifndef _LEFTIST_HEAP
+#define _LEFTIST_HEAP
+
+struct TreeNode;
+typedef TreeNode * PriorityQueue;
+typedef int ElementType;
+
+
+PriorityQueue merge(PriorityQueue H1, PriorityQueue H2);
+ElementType findMin(PriorityQueue H);
+int isEmpty(PriorityQueue H);
+PriorityQueue deleteMin(PriorityQueue H);
+PriorityQueue insert(ElementType X, PriorityQueue H);
+
+void PrintTree(PriorityQueue T);
+void PrintTree(PriorityQueue T, int depth);
+void PrintDepth(ElementType A, int depth);
+
+#endif
+
+struct TreeNode
+{
+	ElementType Element;
+	PriorityQueue Left;
+	PriorityQueue Right;
+	int Npl;
+};
